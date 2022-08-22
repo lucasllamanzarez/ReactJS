a78cbf7049cc43d2a57a0e640b505de3403ea923
lucasllamanzarez <103078227+lucasllamanzarez@users.noreply.github.com>
2022-08-22 13:18:02 -0300

    Se agrega Navbar, se usa "useState" y se modifican algunos estilos

--- react-ecommerce-rt/src/App.js
+++ react-ecommerce-rt/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+//Importar Barra de Navegacion
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Alumno: Lucas Llamanzarez <br></br>
-        Comision: 38060 <br></br>
-        Ecommerce:  Rise Technology <br></br>
-        </p>
-        <a
+      <nav><Navbar /></nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Alumno: Lucas Llamanzarez <br></br>
+          Comision: 38060 <br></br>
+          Ecommerce:  Rise Technology <br></br>
+          </p>
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          >
+            Learn React
+          </a>
       </header>
     </div>
   );
---
+++ react-ecommerce-rt/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import {useState} from 'react'
+import './Navbar.css';
+
+
+const Navbar = () => {
+        
+    //Genero estado del menu
+        const [menu , setMenu] = useState(false)
+           
+        //Genero funcion para ejecutar estado
+            const toggleMenu = () => {
+                setMenu( !menu )
+            }
+
+    return (
+        
+            <div className="barraNav">
+                <h1 className="barraNav-h1">
+                    <a href="#" className="barraNav-a">
+                    Rise Technology
+                    </a>
+                </h1>
+
+                <button onClick={ toggleMenu } className="barraNav-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="barraNav-svg" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                </button>  
+
+                <nav className={ `barraNav-nav ${menu ? 'isActive' : ''}`}>
+                    <ul className="barraNav-ul">
+                        <li className="barraNav-li"><a href="#" className="barraNav-a">Sobre Nosotros</a></li>
+                        <li className="barraNav-li"><a href="#" className="barraNav-a">Productos</a></li>
+                        <li className="barraNav-li"><a href="#" className="barraNav-a">Contactanos</a></li>
+                        <li className="barraNav-li"><a href="#" className="barraNav-a">Carga de productos</a></li>
+                    </ul>
+                </nav>
+
+        </div>
+
+    )
+}
+
+export default Navbar;
